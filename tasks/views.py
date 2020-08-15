183e6d689b4e3f99a778a55ccf6253b3b2a6bdae
Mufaddal1125 <65411218+Mufaddal1125@users.noreply.github.com>
2020-08-15 14:57:23 +0530

    Added Priorities Functionality

--- tasks/views.py
+++ tasks/views.py
@@ -10,29 +10,20 @@ class Forms(forms.Form):
 
 
 def index(request):
-    if "tasks" not in request.session or "priority" not in request.session:
+    if "tasks" not in request.session:
         request.session['tasks'] = []
-        request.session['priority'] = 0
     context = {
         "tasks": request.session['tasks'],
         "form": Forms()
     }
     return render(request, "tasks/index.html", context)
 
-
-def taskssort(e):
-    return e['priority']
-
-
 def add(request):
     if request.method == "POST":
         form = Forms(request.POST)
         if form.is_valid():
-            request.session['priority'] = len(request.session['tasks']) + 1
-            task = {"task": form.cleaned_data['task'], "priority": request.session['priority']}
+            task = form.cleaned_data['task']
             request.session['tasks'] += [task]
-            request.session['tasks'].sort(key=taskssort)
-
     return HttpResponseRedirect(reverse('index'))
 
 def removetask(request):
@@ -41,19 +32,3 @@ def removetask(request):
     del tasks[index]
     request.session['tasks'] = tasks
     return HttpResponseRedirect(reverse('index'))
-
-
-def inc(request, index):
-    tasks = request.session['tasks']
-    tasks[index]["priority"] -= 1
-    request.session['tasks'] = tasks
-    request.session['tasks'].sort(key=taskssort)
-    return HttpResponseRedirect(reverse("index"))
-
-
-def dec(request, index):
-    tasks = request.session['tasks']
-    tasks[index]["priority"] += 1
-    request.session['tasks'] = tasks
-    request.session['tasks'].sort(key=taskssort)
-    return HttpResponseRedirect(reverse("index"))
